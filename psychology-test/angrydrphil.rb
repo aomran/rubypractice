17c5914b1a9a02c7b8ec23af39ca5b28b2cc6dff
Ahmed Omran <aomra015@gmail.com>
2013-07-09 14:57:13 -0400

    tab indentation converted to spaces

--- psychology-test/angrydrphil.rb
+++ psychology-test/angrydrphil.rb
@@ -8,101 +8,101 @@ class AngryDrPhil
 
   private
   def speaker_title
-  	"<%= color('Dr.Phil', BLUE) %>: "
+    "<%= color('Dr.Phil', BLUE) %>: "
   end
 
   def colour_comment(comment)
-  	%Q{ <%= color("#{comment}", RED) %> }
+    %Q{ <%= color("#{comment}", RED) %> }
   end
 
   def speaker_says(comment, color=true)
-  	comment = colour_comment(comment) if color
-  	"#{speaker_title} #{comment} \n"
+    comment = colour_comment(comment) if color
+    "#{speaker_title} #{comment} \n"
   end
 
-	def feeling
-		ask "#{speaker_title} How are you feeling today?"
-		say speaker_says("Your feelings? To he** with your feelings!")
-	end
+  def feeling
+    ask "#{speaker_title} How are you feeling today?"
+    say speaker_says("Your feelings? To he** with your feelings!")
+  end
 
-	def sleep
-		ask "#{speaker_title} How is your sleeping, too much/not enough?"
-	end
+  def sleep
+    ask "#{speaker_title} How is your sleeping, too much/not enough?"
+  end
 
-	def nutrition
-		answer = ask "#{speaker_title} How is your eating, too much/not enough?"
-		if /not enough/ =~ answer
-			 say speaker_says("You need to listen to your body because your body is listening to you.")
-		end
-	end
+  def nutrition
+    answer = ask "#{speaker_title} How is your eating, too much/not enough?"
+    if /not enough/ =~ answer
+       say speaker_says("You need to listen to your body because your body is listening to you.")
+    end
+  end
 
-	def physical_health
-		ask "#{speaker_title} Do you have any medical health issues? Any allergies or medication?"
-	end
+  def physical_health
+    ask "#{speaker_title} Do you have any medical health issues? Any allergies or medication?"
+  end
 
-	def therapy_history
-		ask "#{speaker_title} Have you ever been in therapy before?"
-	end
+  def therapy_history
+    ask "#{speaker_title} Have you ever been in therapy before?"
+  end
 
-	def anxiety
-		answer = ask "#{speaker_title} Do you feel anxious?" do |q|
-			q.validate = /^(yes|no)$/
-		end
-		if answer == "yes"
-			say speaker_says("You need to marry yo' baby mama.")
-		end
-	end
+  def anxiety
+    answer = ask "#{speaker_title} Do you feel anxious?" do |q|
+      q.validate = /^(yes|no)$/
+    end
+    if answer == "yes"
+      say speaker_says("You need to marry yo' baby mama.")
+    end
+  end
 
-	def agitated
-		ask "#{speaker_title} Do you feel agitated?"
-	end
+  def agitated
+    ask "#{speaker_title} Do you feel agitated?"
+  end
 
-	def suicidal
-		answer = ask "#{speaker_title} Have you had suicidal thoughts?" do |q|
-			q.validate = /^(yes|no)$/
-		end
-		if answer == "yes"
-			say speaker_says("You have the duty and gift of living. You don't have the right to sit on the sidelines--use your life and get back into the game.")
-		end
-	end
+  def suicidal
+    answer = ask "#{speaker_title} Have you had suicidal thoughts?" do |q|
+      q.validate = /^(yes|no)$/
+    end
+    if answer == "yes"
+      say speaker_says("You have the duty and gift of living. You don't have the right to sit on the sidelines--use your life and get back into the game.")
+    end
+  end
 
-	def depression_cause
-		ask "#{speaker_title} What makes you depressed (anything specific?)"
-		say speaker_says("Awareness without action is worthless.")
-	end
+  def depression_cause
+    ask "#{speaker_title} What makes you depressed (anything specific?)"
+    say speaker_says("Awareness without action is worthless.")
+  end
 
-	def killing_thoughts
-		answer = ask "#{speaker_title} Have you thought about killing animals or people?" do |q|
-			q.validate = /^(yes|no)$/
-		end
+  def killing_thoughts
+    answer = ask "#{speaker_title} Have you thought about killing animals or people?" do |q|
+      q.validate = /^(yes|no)$/
+    end
 
-		if answer == "yes"
-			choose do |menu|
-				menu.prompt = "So which was it?"
-				menu.choice(:animals) { say speaker_says("You're a terrible person but it's ok.") }
-				menu.choice(:people) { say speaker_says("When you choose your behavior, you choose your consequences.") }
-			end
-		end
-	end
+    if answer == "yes"
+      choose do |menu|
+        menu.prompt = "So which was it?"
+        menu.choice(:animals) { say speaker_says("You're a terrible person but it's ok.") }
+        menu.choice(:people) { say speaker_says("When you choose your behavior, you choose your consequences.") }
+      end
+    end
+  end
 
-	def greet
-		say speaker_says("Hello, this is Dr. Phil. If someone out there doesn't agree with me, then somewhere a village is missing their idiot.", false)
-		say speaker_says("I'll be asking you a few questions now.", false)
-		puts
-	end
+  def greet
+    say speaker_says("Hello, this is Dr. Phil. If someone out there doesn't agree with me, then somewhere a village is missing their idiot.", false)
+    say speaker_says("I'll be asking you a few questions now.", false)
+    puts
+  end
 
-	def ask_questions
-		QUESTIONS.shuffle!
-		QUESTIONS.each do |question|
-			self.send(question)
-		end
-	end
+  def ask_questions
+    QUESTIONS.shuffle!
+    QUESTIONS.each do |question|
+      self.send(question)
+    end
+  end
 
-	public
-	def start_session
-		greet
-		ask_questions
-	end
+  public
+  def start_session
+    greet
+    ask_questions
+  end
 
 end
 
